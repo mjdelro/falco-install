f0b9f223bfe5e6861c1368283cb708aa0808908f
Michael del Rosario <mikejdelro@gmail.com>
2019-02-28 14:35:25 -0800

    cleaned up code

--- sysdig-falco.sh
+++ sysdig-falco.sh
@@ -5,52 +5,38 @@
 #Functions
 
 #Asks the user yes or no
-ask() {
-    local prompt default reply
-    if [ "${2:-}" = "Y" ]; then
-        prompt="Y/n"
-        default=Y
-    elif [ "${2:-}" = "N" ]; then
-        prompt="y/N"
-        default=N
-    else
-        prompt="y/n"
-        default=
-    fi
-    while true; do
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -n "$1 [$prompt] "
-        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-        read reply </dev/tty
-        # Default?
-        if [ -z "$reply" ]; then
-            reply=$default
-        fi
-        # Check if the reply is valid
-        case "$reply" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
+    ask() {
+        # call with a prompt string or use a default
+        read -r -p "${1:-Are you sure? [y/N]} " response
+        case "$response" in
+            [yY][eE][sS]|[yY]) 
+                true
+                ;;
+            *)
+                false
+                ;;
         esac
-    done
-}
+    }
 
 #Asks user to press any key to continue
-function continue {
-    read -p "Press any key to continue or CTRL+C to abort...";
-}
+    function continue {
+        read -p "Press any key to continue or CTRL+C to abort...";
+    }
 
 
 
 #---------#
 #Initial Update/Upgrade
 
-ask "Would you like to upgrade your version of Linux?" && sudo apt-get upgrade -y && clear 
-ask "Would you like to update your version of Linux?" && sudo apt-get update -y && clear
+    ask "Would you like to upgrade your version of Linux?" && sudo apt-get upgrade -y 
+        clear 
+    ask "Would you like to update your version of Linux?"  && sudo apt-get update -y
+        clear
 
 #Install software-properties-common, in case it isn't installed
 #Will hopefully install python3, which I will try and use moving forward.
 #https://packages.ubuntu.com/bionic/software-properties-common
-check "Install software-properties-common" && sudo apt-get install software-properties-common -y 
+    ask "Install software-properties-common?" && sudo apt-get install software-properties-common -y 
 
 
 
@@ -66,23 +52,25 @@ check "Install software-properties-common" && sudo apt-get install software-prop
     continue
 
 #Install Requirements via Apt
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
 #Uninstall previous versions of Docker
-ask "The script will now try to uninstall previous versions of Docker previously installed on your system. Continue?" && sudo apt-get remove docker docker-engine docker.io containerd runc
+    ask "The script will now try to uninstall previous versions of Docker previously installed on your system. Continue?" && sudo apt-get remove docker docker-engine docker.io containerd runc
 
 #Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 #Add Docker's repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 #Update Apt with included Docker repository
-sudo apt-get update
+    sudo apt-get update
 
 #Install the latest version of Docker CE and containerd
-ask "The script will now install docker-ce docker-ce-cli and containerd. Continue?" && sudo apt-get install docker-ce docker-ce-cli containerd.io -y && clear
+    ask "The script will now install docker-ce docker-ce-cli and containerd. Continue?" && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+    clear
 
+#End of Docker script
 echo "The script has finished installing the latest version of Docker"
 continue
 
@@ -91,3 +79,4 @@ continue
 #---------#
 #Installing Falco
 
+ask "The script will now install "
